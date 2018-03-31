eedbc2382573e77b7d70f7fd1fb34ef3a0ae01c0
Achintya Srivastava <achintyasrivastava@Rahuls-MacBook-Pro.local>
2018-04-01 04:29:31 +0530

    Friend list

--- Wowza/src/main/java/com/wowza/controller/Friends_mappingController.java
+++ Wowza/src/main/java/com/wowza/controller/Friends_mappingController.java
@@ -1,28 +1,56 @@
 package com.wowza.controller;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.wowza.model.Friends;
 import com.wowza.model.Friends_mapping;
+import com.wowza.model.User;
 import com.wowza.service.Friends_mappingService;
+import com.wowza.service.UserService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/friendsMapping")
 public class Friends_mappingController {
-	
+
 	private Friends_mappingService friends_mappingService;
-	
+	private UserService userService;
+
 	public Friends_mappingController(Friends_mappingService friends_mappingService) {
 		this.friends_mappingService = friends_mappingService;
 	}
 
 	@GetMapping("/getFriends")
-	public Iterable<Friends_mapping> list(){
+	public Iterable<Friends_mapping> list() {
 		System.out.println("List:");
 		return this.friends_mappingService.list();
 	}
-	
+
 	@PostMapping("/save")
-	public Friends_mapping saveTask(@RequestBody  Friends_mapping friends_mapping) {
+	public Friends_mapping saveTask(@RequestBody Friends_mapping friends_mapping) {
 		return this.friends_mappingService.save(friends_mapping);
 	}
 
+	@GetMapping(value = "/getFriendList/{user_id}")
+	@ResponseBody
+	@JsonIgnoreProperties
+	public Iterable<Friends> getFriendList(@PathVariable("user_id") long user_id) {
+
+//		Iterable<User> users = userService.list();
+//		Iterator<User> userIterator = users.iterator();
+//		Iterable<Friends> friends = null;
+//		while (userIterator.hasNext()) {
+//			System.out.println(userIterator.next());
+//			if (userIterator.next()) {
+//			}
+//			return friends;
+//		}
+		return  this.friends_mappingService.getFriendList(user_id);
+
+	}
 }
--- Wowza/src/main/java/com/wowza/service/Friends_mappingServiceImpl.java
+++ Wowza/src/main/java/com/wowza/service/Friends_mappingServiceImpl.java
@@ -1,11 +1,18 @@
 package com.wowza.service;
 
+import com.wowza.model.Friends;
 import com.wowza.model.Friends_mapping;
 import com.wowza.repository.Friends_mappingRepository;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Friends_mappingServiceImpl implements Friends_mappingService{
+
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	private Friends_mappingRepository friends_mappingRepository;
 
@@ -25,6 +32,14 @@ public class Friends_mappingServiceImpl implements Friends_mappingService{
 	@Override
 	public Friends_mapping save(Friends_mapping friends_mapping) {
 		return this.friends_mappingRepository.save(friends_mapping);
+	}
+
+	@Override
+	public Iterable<Friends> getFriendList(long user_id){
+		Session session = sessionFactory.openSession();
+		String hql = "select F.friend_id FROM F.friends_mapping where F.user_id = '" +user_id+ "' and F.pending = '" +false+ "'";
+		Iterable result = session.createQuery(hql).list();
+		return result;
 	}
 
 }
